7077041cf3a6b74041acc0fabe16ff8f070a4fa3
Natalia Salvino <nataliasalvino86@gmail.com>
2024-07-29 19:22:08 -0300

    project setup

---
+++ bill-processor/src/main/java/bill_processor/App.java
@@ -0,0 +1,13 @@
+package bill_processor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
