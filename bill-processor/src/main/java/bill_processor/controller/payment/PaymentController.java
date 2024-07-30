e952b4a4d970e443e0576665471d4f189080eedd
Natalia Salvino <nataliasalvino86@gmail.com>
2024-07-29 21:34:00 -0300

    refactor: refactoring create payment to validate boleto value

--- bill-processor/src/main/java/bill_processor/controller/payment/PaymentController.java
+++ bill-processor/src/main/java/bill_processor/controller/payment/PaymentController.java
@@ -8,6 +8,10 @@ import java.time.LocalDate;
 public class PaymentController {
 
     public Payment create(Double value, LocalDate date, PaymentTypeEnum type) {
+        if(type.equals(PaymentTypeEnum.BOLETO) && value < 0.01) {
+            throw new IllegalArgumentException("Invalid value");
+        }
+
         return new Payment(value, date, type);
     }
 }
