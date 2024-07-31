6bb826d6f9826bc72a071d5615ad14a4b0b41885
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-07-31 20:36:51 -0300

    refactor: rename controllers to service

--- ticket-system/src/main/java/ticket_system/ticket/service/TicketService.java
+++ ticket-system/src/main/java/ticket_system/ticket/service/TicketService.java
@@ -1,10 +1,11 @@
-package ticket_system.ticket.controllers;
+package ticket_system.ticket.service;
 
+import ticket_system.exceptions.TicketAlreadySoldException;
 import ticket_system.models.Ticket;
 import ticket_system.models.TicketStatus;
 import ticket_system.models.TicketType;
 
-public class TicketController {
+public class TicketService {
 
     public Ticket create(int id, TicketType ticketType, double price) {
         return new Ticket(id, ticketType, price);
--- ticket-system/src/test/java/ticket_system/TicketTest.java
+++ ticket-system/src/test/java/ticket_system/TicketTest.java
@@ -4,19 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ticket_system.exceptions.TicketAlreadySoldException;
 import ticket_system.models.Ticket;
 import ticket_system.models.TicketStatus;
 import ticket_system.models.TicketType;
-import ticket_system.ticket.controllers.TicketController;
+import ticket_system.ticket.service.TicketService;
 
 
 class TicketTest {
     private Ticket notSoldTicket;
+    private TicketService ticketService;
 
     @BeforeEach
     void setup() {
 
         this.notSoldTicket = new Ticket(1, TicketType.VIP, 20.0);
+        this.ticketService = new TicketService();
 
     }
 
@@ -25,22 +28,20 @@ class TicketTest {
         int id = 1;
         TicketType ticketType = TicketType.MEIA_ENTRADA;
         double price = 20.0;
-
         Ticket expectedTicket = new Ticket(id, ticketType, price);
 
-        TicketController ticketController = new TicketController();
-
-        Ticket ticket = ticketController.create(id, ticketType, price );
+        Ticket ticket = this.ticketService.create(id, ticketType, price );
 
         assertEquals(expectedTicket, ticket);
 
     }
 
+
     @Test
-    void shouldUpdateTicketStatusWhenSellTicket() {
+    void shouldUpdateTicketStatusWhenSellTicket() throws TicketAlreadySoldException {
         assertEquals(TicketStatus.NOT_SOLD, this.notSoldTicket.getTicketStatus());
 
-        this.notSoldTicket.sell();
+        this.ticketService.sellTicket(this.notSoldTicket);
 
         assertEquals(TicketStatus.SOLD, this.notSoldTicket.getTicketStatus());
 
