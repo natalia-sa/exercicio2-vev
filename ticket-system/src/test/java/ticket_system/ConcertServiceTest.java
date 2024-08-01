1a83d445843d45d964c1f19a5443122cb0bfaa2d
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-07-31 22:44:15 -0300

    tests: should add ticket lot to concert

--- ticket-system/src/test/java/ticket_system/ConcertServiceTest.java
+++ ticket-system/src/test/java/ticket_system/ConcertServiceTest.java
@@ -3,19 +3,24 @@ package ticket_system;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ticket_system.models.Concert;
+import ticket_system.models.TicketLot;
 import ticket_system.service.ConcertService;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConcertServiceTest {
 
     private ConcertService concertService;
+    private Concert specialDateConcert;
+
 
     @BeforeEach
     void setup() {
         this.concertService = new ConcertService();
+        this.specialDateConcert = new Concert( LocalDate.of(2020, 12, 10), "Japãozinho", 2000, 2000, true);
     }
 
     @Test
@@ -31,6 +36,17 @@ public class ConcertServiceTest {
         Concert concert = this.concertService.create(date, artist, cache, infrastructureCosts, isInSpecialDate);
 
         assertEquals(concert, expectedConcert);
+    }
+
+    @Test
+    void shouldAddTicketLotToConcert() {
+        assertEquals(0, this.specialDateConcert.getTicketLots().size());
+
+        TicketLot ticketLot = new TicketLot(1, new ArrayList<>(), 0.1);
+        this.concertService.addTicketLotToConcert(this.specialDateConcert, ticketLot);
+
+        assertEquals(1, this.specialDateConcert.getTicketLots().size());
+
     }
 
 
