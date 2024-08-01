f4914ecfe153f8760a68385af40a3cb9f4f2165a
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-07-31 21:40:25 -0300

    tests: should throw exception case vip tickets are lower than 20% of total tickets

--- ticket-system/src/test/java/ticket_system/TicketLotServiceTest.java
+++ ticket-system/src/test/java/ticket_system/TicketLotServiceTest.java
@@ -2,20 +2,26 @@ package ticket_system;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ticket_system.exceptions.TicketAlreadySoldException;
 import ticket_system.models.Concert;
 import ticket_system.models.Ticket;
 import ticket_system.models.TicketLot;
+import ticket_system.models.TicketType;
 import ticket_system.service.ConcertService;
 import ticket_system.service.TicketLotService;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TicketLotServiceTest {
     private TicketLotService ticketLotService;
+    private Ticket vipTicket;
+    private Ticket meiaEntradaTicket;
 
     @BeforeEach
     void setup() {
@@ -44,6 +50,38 @@ public class TicketLotServiceTest {
         TicketLot ticketLot = this.ticketLotService.create(id, tickets, applicableDiscount);
 
         assertEquals(0.25, ticketLot.getApplicableDiscount());
+    }
+
+    @Test
+    void shouldThrowExceptionCaseVipTicketsIsLowerThan20Percent() {
+        // 19 Vip Tickets, 10 Meia entrada tickets, 71 normal tickets. Total of 100 tickets and vip tickets are 19%
+        List<Ticket> vipTickets = this.generateTickets(19, TicketType.VIP); // 19 Vip tickets
+        List<Ticket> meiaEntradaTickets = this.generateTickets(10, TicketType.VIP); // 10 meia entrada tickets
+        List<Ticket> normalTickets = this.generateTickets(71, TicketType.VIP); // 71 meia entrada tickets
+
+        List<Ticket> allTickets = new ArrayList<>();
+
+        allTickets.addAll(vipTickets);
+        allTickets.addAll(meiaEntradaTickets);
+        allTickets.addAll(normalTickets);
+
+        int id = 1;
+        double applicableDiscount = 0.1;
+
+        assertThrows(TicketLotConfigurationException.class, ()->{
+            this.ticketLotService.create(id, allTickets, applicableDiscount);
+        });
+
+    }
+
+    private List<Ticket> generateTickets(int quantity, TicketType type) {
+        List<Ticket> tickets = new ArrayList<>();
+
+        for (int i = 0; i < quantity ; i++) {
+            tickets.add(new Ticket(i, type, 20));
+        }
+
+        return tickets;
     }
 
 
