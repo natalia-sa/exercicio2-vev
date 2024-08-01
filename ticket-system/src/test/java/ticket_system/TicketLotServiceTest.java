4e1eb4c79e6d2cd22a24b821de3bd894d2e2eee1
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-07-31 21:11:13 -0300

    tests: adding tests to create a lot

---
+++ ticket-system/src/test/java/ticket_system/TicketLotServiceTest.java
@@ -0,0 +1,38 @@
+package ticket_system;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ticket_system.models.Concert;
+import ticket_system.models.Ticket;
+import ticket_system.models.TicketLot;
+import ticket_system.service.ConcertService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TicketLotServiceTest {
+    private TicketLotService ticketLotService;
+
+    @BeforeEach
+    void setup() {
+        this.ticketLotService = new TicketLotService();
+    }
+
+    @Test
+    void shouldCreateConcert() {
+        int id = 1;
+        List<Ticket> tickets = new ArrayList<Ticket>();
+        double applicableDiscount = 0.5;
+
+        TicketLot expectedTicketLot = new TicketLot(id, tickets, applicableDiscount);
+
+        TicketLot ticketLot = this.ticketLotService.create();
+
+        assertEquals(ticketLot, expectedTicketLot);
+    }
+
+
+}
